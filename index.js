7e91cff0446e6394d5f38c80be4846f9be154d5a
Nora <nhalmarri64@gmail.com>
2024-10-07 11:03:23 +0300

    BMI Status + Calculater

--- index.js
+++ index.js
@@ -8,4 +8,37 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+
+ 
+  let convHeight = height/100;
+
+  let bmi = (weight/convHeight^2);
+
+  alert(bmi)
+
+  //BMI Status
+
+  if( bmi < 18.5){
+    alert("Underweight");
+    alert("Not Healthy");
+  } else if (bmi > 18.5 && bmi < 24.9 && age >= 19 && age <= 24){
+    alert("Healthy Weight");
+    alert("Healthy");
+
+  } else if( bmi > 25.0 && bmi < 29.9 && age >= 25 && age <= 65){
+    alert("Overweight");
+    alert("Healthy");
+  } else{
+    alert("Above Obesity");
+    alert("Not Healthy");
+    
+  }
+
+  
+  // BMI by Age 
+
+
+
+
+
 }
